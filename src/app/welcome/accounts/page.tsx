a2b333735ae357dccf49f2e4d604aba54146a6c2
Codebulous <josephemmanuelchay@gmail.com>
2024-08-17 06:12:54 +0800

    some library cleanup

--- src/app/welcome/accounts/page.tsx
+++ src/app/welcome/accounts/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import InputGroup from "@/components/FormElements/InputGroup";
 
 const GoogleAccountButton = () => {
