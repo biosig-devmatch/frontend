686ea065cfb950cca0b5d51a005d1e6cc7e3baf5
cheongyeechian <cheongyeechian@gmail.com>
2024-08-17 10:42:39 +0800

    move folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import { ThirdwebProvider, ConnectEmbed, ConnectButton, } from "thirdweb/react";
-import Header from "@/components/Header/Header";
+import Header from "./components/Header/Header";
 import { cn } from "@/lib/utils"
 
 
